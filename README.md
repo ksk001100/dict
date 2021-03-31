00d3d34fb5a8426de37b59a6bc36b8933ab8cf06
KeisukeToyota <hm.pudding0715@gmail.com>
2021-03-31 21:23:52 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# dict
+
+Dictionary CLI to search from Wikipedia
+
+# Install
+```bash
+$ git clone https://github.com/ksk001100/dict
+$ cd dict
+$ cargo install --path .
+```
+
+# Usage
+```bash
+$ dict macOS
+$ dict macOS --lang en
+$ dict macOS -l en
+$ dict --help
+```
---
+++ src/main.rs
@@ -0,0 +1,106 @@
+use async_std::task;
+use seahorse::{App, Context, Flag, FlagType};
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+use std::env;
+use std::process::exit;
+use surf::http::Url;
+
+#[derive(Serialize, Deserialize)]
+struct Params<'a> {
+    format: &'a str,
+    action: &'a str,
+    prop: &'a str,
+    exintro: bool,
+    explaintext: bool,
+    redirects: &'a str,
+    titles: &'a str,
+}
+
+#[derive(Deserialize, Debug)]
+struct PageValue {
+    title: String,
+    extract: String,
+}
+
+#[derive(Deserialize, Debug)]
+struct Query {
+    pages: HashMap<String, PageValue>,
+}
+
+#[derive(Deserialize, Debug)]
+struct Response {
+    query: Query,
+}
+
+async fn search(lang: String, text: String) -> surf::Result<()> {
+    let url = Url::parse(&format!("https://{}.wikipedia.org/w/api.php", lang))?;
+
+    let params = Params {
+        format: "json",
+        action: "query",
+        prop: "extracts",
+        exintro: true,
+        explaintext: true,
+        redirects: "1",
+        titles: &text,
+    };
+
+    let res = if let Ok(res) = surf::get(url).query(&params)?.recv_string().await {
+        res
+    } else {
+        eprintln!("That language does not exist");
+        exit(1);
+    };
+
+    let res: Response = match serde_json::from_str(&res) {
+        Ok(res) => res,
+        Err(_) => {
+            eprintln!("Not found...");
+            exit(1);
+        }
+    };
+
+    match res.query.pages.values().next() {
+        Some(page) => println!("{}", page.extract),
+        None => eprintln!("Not found..."),
+    }
+
+    Ok(())
+}
+
+fn action(c: &Context) {
+    task::block_on(async {
+        let text = if let Some(text) = c.args.get(0) {
+            text
+        } else {
+            c.help();
+            exit(1);
+        };
+
+        let lang = match c.string_flag("lang") {
+            Ok(lang) => lang,
+            Err(_) => match env::var("LANG") {
+                Ok(ref lang) => lang[..2].to_string(),
+                Err(_) => "en".to_string(),
+            },
+        };
+
+        search(lang, text.to_owned()).await.unwrap();
+    });
+}
+
+fn main() {
+    App::new(env!("CARGO_PKG_NAME"))
+        .description(env!("CARGO_PKG_DESCRIPTION"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .version(env!("CARGO_PKG_VERSION"))
+        .usage(format!("{} [text]", env!("CARGO_PKG_NAME")))
+        .action(action)
+        .flag(
+            Flag::new("lang", FlagType::String)
+                .alias("l")
+                .description("Language designation"),
+        )
+        .run(env::args().collect())
+}
